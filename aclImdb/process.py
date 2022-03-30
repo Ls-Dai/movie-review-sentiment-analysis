67c278f6719699ac4767ee4a7fcaea638d6be390
Lisen Dai <lisendai0@gmail.com>
2022-03-30 16:30:11 -0400

    update

---
+++ aclImdb/process.py
@@ -0,0 +1,69 @@
+import os
+import pandas as pd
+
+
+def main():
+
+    # train
+    # pos
+    file_names = os.listdir("./train/pos")
+    for i in range(len(file_names)):
+        file_names[i] = "./train/pos/" + file_names[i]
+
+    reviews = []
+
+    for file_name in file_names:
+        with open(file_name, 'r') as f:
+            review = f.readline()
+            reviews.append((review, 'positive'))
+
+    # train
+    # neg
+    file_names = os.listdir("./train/neg")
+    for i in range(len(file_names)):
+        file_names[i] = "./train/neg/" + file_names[i]
+
+    reviews = []
+
+    for file_name in file_names:
+        with open(file_name, 'r') as f:
+            review = f.readline()
+            review = review.replace("<br />", '')
+            reviews.append((review, 'negative'))
+
+    df = pd.DataFrame(reviews, columns=['review', 'sentiment'])
+    df.to_csv('train.csv', index=False)
+
+    # test
+    # pos
+    file_names = os.listdir("./test/pos")
+    for i in range(len(file_names)):
+        file_names[i] = "./test/pos/" + file_names[i]
+
+    reviews = []
+
+    for file_name in file_names:
+        with open(file_name, 'r') as f:
+            review = f.readline()
+            reviews.append((review, 'positive'))
+
+    # test
+    # neg
+    file_names = os.listdir("./test/neg")
+    for i in range(len(file_names)):
+        file_names[i] = "./test/neg/" + file_names[i]
+
+    reviews = []
+
+    for file_name in file_names:
+        with open(file_name, 'r') as f:
+            review = f.readline()
+            review = review.replace("<br />", '')
+            reviews.append((review, 'negative'))
+
+    df = pd.DataFrame(reviews, columns=['review', 'sentiment'])
+    df.to_csv('test.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
