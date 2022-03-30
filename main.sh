756c8f9da30a0216e434c1e32f3d7fa0f46004dd
Lisen Dai <lisendai0@gmail.com>
2022-03-29 23:42:36 -0400

    Initial Commit

---
+++ main.sh
@@ -0,0 +1,1 @@
+python run.py --train --path "./data/IMDB Dataset"
---
+++ run.py
@@ -0,0 +1,62 @@
+import argparse
+import torch
+from torch.utils.data import RandomSampler
+import os
+from transformers import BertConfig, BertForSequenceClassification, BertTokenizer
+
+from dataset import Dataset
+from model import BertEstimator 
+
+
+BERT_MODEL = 'bert-base-uncased'
+NUM_LABELS = 2  # negative and positive reviews
+
+
+def train(input_path, epochs=10, output_path="./weights/"):
+    config = BertConfig.from_pretrained(BERT_MODEL, num_labels=NUM_LABELS)
+    tokenizer = BertTokenizer.from_pretrained(BERT_MODEL, do_lower_case=True)
+
+    model = BertForSequenceClassification.from_pretrained(BERT_MODEL, config=config)
+
+    dataset = Dataset(tokenizer=tokenizer)
+    dataloader = dataset.prepare_dataloader(input_path, sampler=RandomSampler)
+    estimator = BertEstimator()
+    estimator.train(
+        tokenizer=tokenizer,
+        dataloader=dataloader,
+        model=model,
+        epochs=epochs, 
+    )
+    model.save_pretrained(output_path)
+    tokenizer.save_pretrained(output_path)
+
+
+def evaluate(input_path, epochs=10, output_path="./weights/"):
+    pass
+
+
+def main():
+
+    parser = argparse.ArgumentParser(prog='main')
+
+    parser.add_argument("--train", action="store_true")
+    parser.add_argument("--evaluate", action="store_true")
+    parser.add_argument("--path", default="", type=str)
+    parser.add_argument("--epochs", default=10, type=int)
+
+    args = parser.parse_args()
+
+    if args.train:
+        if os.path.exists(args.path):
+            train(input_path=args.path, epochs=args.epochs)
+        else:
+            print("Not a valid data path.")
+    elif args.evaluate:
+        if os.path.exists(args.path):
+            evaluate()
+        else:
+            print("Not a valid data path.")
+
+
+if __name__ == "__main__":
+    main()
